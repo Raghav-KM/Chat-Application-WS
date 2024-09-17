1fec1b04dde12c0fcfe83656363a51b4cb29f98d
Raghav <Raghav-KM>
2024-09-17 17:19:49 +0530

    Frontent Init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,15 +9,15 @@ const httpServer = app.listen(3000, () => {
 const wss = new WebSocketServer({ server: httpServer });
 
 wss.on("connection", function connection(ws) {
-    ws.on("error", console.error);
+    //     ws.on("error", console.error);
 
-    ws.on("message", function message(data, isBinary) {
-        wss.clients.forEach(function each(client) {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(data, { binary: isBinary });
-            }
-        });
-    });
+    //     ws.on("message", function message(data, isBinary) {
+    //         wss.clients.forEach(function each(client) {
+    //             if (client.readyState === WebSocket.OPEN) {
+    //                 client.send(data, { binary: isBinary });
+    //             }
+    //         });
+    //     });
 
     ws.send("Hello! Message From Server!!");
 });
